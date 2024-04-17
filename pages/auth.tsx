9e22dadef61a73ccabef82627042e722ac1372b1
martleru <68188509+martleru@users.noreply.github.com>
2024-04-17 11:19:00 +0300

    Add: Added next-auth with login/register variants

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,12 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
-
 "use client";
 
 import axios from "axios";
 import Input from "@/components/Input";
 import { useCallback, useState } from "react";
-// import { signIn } from "next-auth/react";
+import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
+
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 
 const Auth = () => {
   const router = useRouter();
@@ -23,34 +25,34 @@ const Auth = () => {
     );
   }, []);
 
-  // const login = useCallback(async () => {
-  //   try {
-  //     await signIn("credentials", {
-  //       email,
-  //       password,
-  //       redirect: false,
-  //       callbackUrl: "/",
-  //     });
+  const login = useCallback(async () => {
+    try {
+      await signIn("credentials", {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: "/",
+      });
 
-  //     router.push("/");
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }, [email, password, router]);
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, password, router]);
 
-  // const register = useCallback(async () => {
-  //   try {
-  //     await axios.post("http://localhost:3000/api/auth/register", {
-  //       email,
-  //       name,
-  //       password,
-  //     });
+  const register = useCallback(async () => {
+    try {
+      await axios.post("http://localhost:3000/api/register", {
+        email,
+        name,
+        password,
+      });
 
-  //     login();
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }, [email, name, password, login]);
+      login()
+    } catch (error) {
+      console.log(error);
+    }
+  }, [email, name, password, login]);
 
   return (
     <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
@@ -89,11 +91,19 @@ const Auth = () => {
               />
             </div>
             <button
-              // onClick={variant === "login" ? login : register}
+              onClick={variant === "login" ? login : register}
               className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition"
             >
               {variant === "login" ? "Login" : "Sign up"}
             </button>
+            <div className="flex flex-row items-center gap-4 mt-8 justify-center">
+              <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FcGoogle size={30} />
+              </div>
+              <div className="w-10 h-10 bg-white rounded-full flex items-center justify-center cursor-pointer hover:opacity-80 transition">
+                <FaGithub size={30} />
+              </div>
+            </div>
             <p className="text-neutral-500 mt-12 text-sm">
               {variant === "login"
                 ? "First time using Netflix?"
